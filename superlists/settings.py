b94c40cf44c535fba0d3cc2aa4c3d11e4d6363b3
Rainer Wagner-Ballner <rainer.wagner-ballner@alphasystems.com>
2022-07-11 14:12:18 +0200

    set up envvars

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$caq&2uf3=-pyr@b3d49jl$3*-glj6o$o#p_)j00$men-s7)cq'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = '$caq&2uf3=-pyr@b3d49jl$3*-glj6o$o#p_)j00$men-s7)cq'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
